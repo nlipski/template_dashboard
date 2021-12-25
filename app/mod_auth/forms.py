d826938454e534d07431b3694cad171955146545
Nick Lipski <rechnaya5@gmail.com>
2021-12-24 22:08:51 -0500

    Temporarely removing Recaptcha field and reenabling sign in requirement

--- app/mod_auth/forms.py
+++ app/mod_auth/forms.py
@@ -11,7 +11,7 @@ from wtforms.validators import DataRequired, EqualTo, Email
 class LoginForm(FlaskForm):
     email    = StringField('Email Address', [Email(), DataRequired(message='Forgot your email?')])
     password = PasswordField('Password', [DataRequired(message='Forgot your password')])
-    recaptcha = RecaptchaField()
+    # recaptcha = RecaptchaField()
     remember = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
@@ -20,6 +20,6 @@ class SignupForm(FlaskForm):
     email    = StringField('Email Address', [Email(), DataRequired()])
     username = StringField('User Name', [DataRequired()])
     password = PasswordField('Password', [DataRequired()])
-    recaptcha = RecaptchaField()
+    # recaptcha = RecaptchaField()
     submit = SubmitField('Sign Up')
 
